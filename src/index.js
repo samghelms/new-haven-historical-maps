d8a000951fbbbd8368cb6ddab32b5db37ef4faaf
Sam <samghelms@gmail.com>
2018-02-19 23:46:22 -0500

    initial

---
+++ src/index.js
@@ -0,0 +1,55 @@
+
+proj4.defs("EPSG:26915", "+proj=utm +zone=15 +ellps=GRS80 +datum=NAD83 +units=m +no_defs");
+
+var coordConverter = proj4('EPSG:3857','EPSG:26915')
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoic2FtZ2hlbG1zIiwiYSI6ImNqNm1qamZ0NDEzbzEycHBkejkwcGd4ZTMifQ.ccWemgAM6or4b6WgxSOtbQ'
+var beforeMap = new mapboxgl.Map({
+    container: 'before',
+    style: 'mapbox://styles/mapbox/satellite-v9',
+    center: [-72.9279, 41.3083],
+    zoom: 13,
+});
+
+var afterMap = new mapboxgl.Map({
+    container: 'after',
+    style: 'mapbox://styles/mapbox/satellite-v9',
+    center: [-72.9279, 41.3083],
+    zoom: 13,
+});
+
+var map = new mapboxgl.Compare(beforeMap, afterMap, {
+    // Set this to enable comparing two maps by mouse movement:
+    // mousemove: true
+});
+
+afterMap.addControl(new mapboxgl.NavigationControl({showCompass: false}));
+
+
+var database = firebase.database();
+
+
+var addLayer = function(map, source, id) { 
+    if(map.getSource(id) === undefined) {
+        map.addSource(id, {
+            type: 'raster',
+            tiles: [source],
+            'tileSize': 256
+        });
+    }
+    console.log("adding: ")
+    console.log(id)
+    map.addLayer({
+        'id': id,
+        'type': 'raster',
+        'source': id,
+        'paint': {}
+    });
+}
+
+afterMap.on('load', function() {
+    addLayer(afterMap, dataSource.data, dataSource.id)
+});
+
+
+
